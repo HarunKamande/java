3ec9a58ac05ff841e057166448b7045b86ecfd1b
harun <kamandeharun99@mail.com>
2024-04-23 06:14:04 +0300

    update

--- Arraylists.java
+++ Arraylists.java
@@ -7,7 +7,16 @@ public class Arraylists {
         ArrayList<Integer> ages=new ArrayList<>();
         ages.add(23);
         ages.add(56);
+<<<<<<< HEAD
         ages.add(0,211);//inserting element to a specific index
         System.out.println(ages);
+=======
+        ages.add(0,211);//inserting element into a specific index
+
+        //looping arraylist
+        for(int i=0; i<ages.size(); i++){
+            System.out.println(ages.get(i));
+        }
+>>>>>>> 703cc5a (update)
     }
 }
